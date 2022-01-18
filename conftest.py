185d662c32223ebe6219f8bc2c52a048de0d2222
Aspergean <UnknownAsperger1>
2022-01-19 00:55:48 +0300

    Added ftp server and environment for that

--- conftest.py
+++ conftest.py
@@ -1,11 +1,9 @@
 from fixture.application import Application
-# from fixture.db import DbFixture
-# from fixture.orm import ORMFixture
 import pytest
 import json
 import os.path
 import importlib
-# import jsonpickle
+import ftputil
 
 fixture = None
 target = None
@@ -21,37 +19,45 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     global target
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))
+    web_config = config
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["web"]["baseUrl"])
-    fixture.session.ensure_login(username=web_config["webadmin"]["username"], password=web_config["webadmin"]["password"])
+    # fixture.session.ensure_login(username=web_config["webadmin"]["username"], password=web_config["webadmin"]["password"])
     return fixture
 
 
-# @pytest.fixture(scope="session")
-# def db(request):
-#     db_config = load_config(request.config.getoption("--target"))["db"]
-#     dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
-#     def fin():
-#         dbfixture.destroy()
-#     request.addfinalizer(fin)
-#     return dbfixture
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
 
 
-# @pytest.fixture(scope="session")
-# def orm(request):
-#     db_config = load_config(request.config.getoption("--target"))["db"]
-#     ormfixture = ORMFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
-#     return ormfixture
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    request.addfinalizer(fin)
 
 
-# @pytest.fixture
-# def check_ui(request):
-#     return request.config.getoption("--check_ui")
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -68,20 +74,3 @@ def pytest_addoption(parser):
     parser.addoption("--check_ui", action="store_true")  # true if exist
 
 
-# def pytest_generate_tests(metafunc):
-#     for fixture in metafunc.fixturenames:
-#         if fixture.startswith("data_"):
-#             testdata = load_from_module(fixture[5:])
-#             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-#         elif fixture.startswith("json_"):
-#             testdata = load_from_json(fixture[5:])
-#             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-
-
-def load_from_module(module):
-    return importlib.import_module("data.%s" % module).testdata
-
-
-# def load_from_json(file):
-#     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-#         return jsonpickle.decode(f.read())
